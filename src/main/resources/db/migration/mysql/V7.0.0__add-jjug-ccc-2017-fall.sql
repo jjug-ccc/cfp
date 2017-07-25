2529097b171efe36d0c609a6f0abba32feef25ad
making <makingx@gmail.com>
2017-07-26 00:28:53 +0900

    Update for JJUG CCC 2017 Fall

---
+++ src/main/resources/db/migration/mysql/V7.0.0__add-jjug-ccc-2017-fall.sql
@@ -0,0 +1,4 @@
+INSERT INTO conference (conf_id, conf_date, conf_name, conf_status, conf_cfp_note, conf_vote_note)
+VALUES (0x00000000000000000000000020171118, '2017-11-18', 'JJUG CCC 2017 Fall', 0,
+        '2017年11月18日(土)に行われるJJUG CCC 2017 FallのCfPを下記フォームをご入力の上、ご提出ください。  \n提出にはGitHubによるログインが必須です。  \n \n#### 締め切り  \n \n2017年2月26日(日) 23:59 GMT \n#### CfP作成方法  \n \nフォーム入力後に"Submit CFP"ボタンクリックで提出完了です。"Save as Draft"ボタンをクリックした場合は提出とみなされません。  \nGitHubで[ログイン](/)後、応募済みのCfPを編集可能です。  \nCfPを取り下げたい場合は"Withdraw CFP"ボタンをクリックしてください。  \n \n#### 重要  \n \n今回は投票による希望セッションのアンケートを**実施しません**。',
+        '今回は投票による希望セッションのアンケートを**実施しません**。');
