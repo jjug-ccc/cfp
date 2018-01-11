bb4251a74a6c4edbe7548b4457fa946b7e0d139d
Ryosuke Uchitate <b1a9id@users.noreply.github.com>
2018-01-12 03:19:44 +0900

    Show updated date (#11)
    fixes #10

--- src/main/java/jjug/vote/VoteSummary.java
+++ src/main/java/jjug/vote/VoteSummary.java
@@ -1,11 +1,12 @@
 package jjug.vote;
 
-import java.util.UUID;
-
 import jjug.submission.enums.Category;
 import jjug.submission.enums.Level;
 import jjug.submission.enums.SubmissionStatus;
 import jjug.submission.enums.TalkType;
+
+import java.time.Instant;
+import java.util.UUID;
 
 public interface VoteSummary {
 	UUID getSubmissionId();
@@ -25,6 +26,8 @@ public interface VoteSummary {
 	TalkType getTalkType();
 
 	SubmissionStatus getStatus();
+
+	Instant getUpdatedAt();
 
 	long getCount();
 }
