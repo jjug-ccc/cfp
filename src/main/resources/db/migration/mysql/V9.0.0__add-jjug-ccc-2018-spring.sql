44ea7356def28cf370eb6acd71db4c9046bc3f10
Toshiaki Maki <tmaki@pivotal.io>
2018-02-08 21:14:55 +0900

    Add JJUG CCC 2018 Spring

---
+++ src/main/resources/db/migration/mysql/V9.0.0__add-jjug-ccc-2018-spring.sql
@@ -0,0 +1,4 @@
+INSERT INTO conference (conf_id, conf_date, conf_name, conf_status, conf_cfp_note, conf_vote_note)
+VALUES (0x00000000000000000000000020180526, '2018-05-26', 'JJUG CCC 2018 Spring', 0,
+        '2018年05月26日(土)に行われるJJUG CCC 2018 SpringのCfPを下記フォームをご入力の上、ご提出ください。  \n提出にはGitHubによるログインが必須です。  \n \n#### 締め切り  \n \n2018年3月25日(日) 23:59 JST \n#### CfP作成方法  \n \nフォーム入力後に"Submit CFP"ボタンクリックで提出完了です。"Save as Draft"ボタンをクリックした場合は提出とみなされません。  \nGitHubで[ログイン](/)後、応募済みのCfPを編集可能です。  \nCfPを取り下げたい場合は"Withdraw CFP"ボタンをクリックしてください。  \n \n#### 重要  \n \n今回は投票による希望セッションのアンケートを**実施しません**。',
+        '投票による希望セッションのアンケートを**実施しません**。');
