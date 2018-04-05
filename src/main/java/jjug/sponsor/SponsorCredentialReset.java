699968adeabb9446deb248b6c52881964741f7a9
Toshiaki Maki <tmaki@pivotal.io>
2018-04-05 22:18:29 +0900

    Extend expiry #45

--- src/main/java/jjug/sponsor/SponsorCredentialReset.java
+++ src/main/java/jjug/sponsor/SponsorCredentialReset.java
@@ -42,6 +42,6 @@ public class SponsorCredentialReset {
 	}
 
 	public Instant expiry() {
-		return this.createdAt.plus(3, ChronoUnit.DAYS);
+		return this.createdAt.plus(14, ChronoUnit.DAYS);
 	}
 }
