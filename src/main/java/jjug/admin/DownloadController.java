67a6933db4f5fec84bab338321760dbe97c0cc5d
Toshiaki Maki <tmaki@pivotal.io>
2018-04-09 13:26:14 +0900

    Add csv download fixes #48

---
+++ src/main/java/jjug/admin/DownloadController.java
@@ -0,0 +1,56 @@
+package jjug.admin;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.UUID;
+
+import jjug.conference.Conference;
+import jjug.conference.ConferenceRepository;
+import jjug.speaker.Speakers;
+import jjug.submission.Submission;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DownloadController {
+	private final Charset windows31j = Charset.forName("Windows-31J");
+	private final MediaType textCsv = new MediaType("text", "csv", windows31j);
+	private final ConferenceRepository conferenceRepository;
+
+	public DownloadController(ConferenceRepository conferenceRepository) {
+		this.conferenceRepository = conferenceRepository;
+	}
+
+	@GetMapping(path = "admin/conferences/{confId}.csv")
+	public ResponseEntity download(@PathVariable UUID confId) throws IOException {
+		Path tempFile = Files.createTempFile("download-", ".csv");
+		File file = tempFile.toFile();
+		try (CSVPrinter csvPrinter = CSVFormat.RFC4180.withHeader("Status", "Title",
+				"Name", "Github", "Email", "Category", "Level", "Type", "Language")
+				.print(file, windows31j)) {
+			Conference conference = conferenceRepository.findOne(confId).get();
+			List<Submission> sessions = conference.getSessions();
+			for (Submission submission : sessions) {
+				Speakers speakers = submission.speakers();
+				csvPrinter.printRecord(submission.getSubmissionStatus(),
+						submission.getTitle(), speakers.getName(), speakers.getGithub(),
+						speakers.getEmail(), submission.getCategory(),
+						submission.getLevel(), submission.getTalkType(),
+						submission.getLanguage());
+			}
+			return ResponseEntity.ok().contentType(textCsv)
+					.body(new FileSystemResource(file));
+		}
+	}
+}
--- src/main/java/jjug/speaker/Speakers.java
+++ src/main/java/jjug/speaker/Speakers.java
@@ -1,38 +1,34 @@
 package jjug.speaker;
 
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Speakers implements Iterable<Speaker> {
-    private List<Speaker> speakers;
+	private List<Speaker> speakers;
 
-    public Speakers(List<Speaker> speakers) {
-        this.speakers = speakers == null ? new ArrayList<>() : speakers;
-    }
+	public Speakers(List<Speaker> speakers) {
+		this.speakers = speakers == null ? new ArrayList<>() : speakers;
+	}
 
-    public String getName() {
-        return this.speakers.stream()
-                .map(Speaker::getName)
-                .collect(Collectors.joining("／"));
-    }
+	public String getName() {
+		return this.speakers.stream().map(Speaker::getName)
+				.collect(Collectors.joining(","));
+	}
 
-    public String getGithub() {
-        return this.speakers.stream()
-                .map(Speaker::getGithub)
-                .collect(Collectors.joining("／"));
-    }
+	public String getGithub() {
+		return this.speakers.stream().map(Speaker::getGithub)
+				.collect(Collectors.joining(","));
+	}
 
-    public String getEmail() {
-        return this.speakers.stream()
-                .map(Speaker::getEmail)
-                .collect(Collectors.joining("／"));
-    }
+	public String getEmail() {
+		return this.speakers.stream().map(Speaker::getEmail)
+				.collect(Collectors.joining(","));
+	}
 
-    @Override
-    public Iterator<Speaker> iterator() {
-        return this.speakers.iterator();
-    }
+	@Override
+	public Iterator<Speaker> iterator() {
+		return this.speakers.iterator();
+	}
 }
