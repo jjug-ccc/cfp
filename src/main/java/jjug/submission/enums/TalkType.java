b52f39e27d6b3a755689b7c7c0bc3244f5c4e657
Toshiaki Maki <tmaki@pivotal.io>
2019-07-11 11:26:13 +0900

    Add stepup talk types
    closes #60

--- src/main/java/jjug/submission/enums/TalkType.java
+++ src/main/java/jjug/submission/enums/TalkType.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TalkType implements DisplayMessage {
-	STANDARD(0), SHORT(5), BEGINNER_STANDARD(6), BEGINNER_SHORT(7), WORKSHOP(10), LT(20);
+	STANDARD(0), SHORT(5), BEGINNER_STANDARD(6), BEGINNER_SHORT(7), WORKSHOP(10), STEPUP_STANDARD(15), STEPUP_SHORT(16), LT(20);
 	private final int value;
 
 	public static TalkType valueOf(int v) {
